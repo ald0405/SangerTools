75a0ee7ca26762b37d5821edc26197a79bc7ff1d
ald0405 <38423717+ald0405@users.noreply.github.com>
2022-01-04 17:15:04 +0000

    Remove Example
    VM does not have clipboard and is causing errors

--- R/excel_clip.R
+++ R/excel_clip.R
@@ -6,17 +6,6 @@
 #' @param col.names Set to TRUE for col.names to be included
 #' @param ... function forwarding for additional write.table functionality
 #' @return a data frame copied to your clipboard
-#' @examples
-#' \donttest{
-#' library(SangerTools)
-#' library(dplyr)
-#' # Group by Age Band
-#' health_data <- SangerTools::PopHealthData
-#' health_data %>%
-#'  slice(1:10) %>%
-#'  SangerTools::excel_clip(row.names = FALSE, col.names = TRUE)
-#'  # Message: Your Item is Now Copied to Clipboard
-#'  }
 #' @export
 
 excel_clip <- function(x,
