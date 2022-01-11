6cb7888be6930c067569f73d16245df30e8bac4e
ald0405 <38423717+ald0405@users.noreply.github.com>
2022-01-11 16:35:05 +0000

    compare_standardised_rates code formatting
    Code spacing

--- R/compare_standardised_rates.R
+++ R/compare_standardised_rates.R
@@ -13,7 +13,15 @@
 #' @importFrom magrittr %>%
 #' @importFrom rlang :=
 #' @export
-compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Condition, Population_Standard = NULL, Verbose = TRUE,...){
+compare_standardised_rates <-
+  function(df,
+           Geography,
+           LocalityA,
+           LocalityB,
+           Condition,
+           Population_Standard = NULL,
+           Verbose = TRUE,
+           ...) {
 
   group_var <- enquos(...)
   # Standard Population
@@ -33,9 +41,9 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
       group_by(!!!group_var) %>%
       count(name = "Pop_Weight")
 
-    overall_pop_sum <-pop_standard %>%
+    overall_pop_sum <- pop_standard %>%
       ungroup() %>%
-      summarise(total_pop = sum(Pop_Weight,na.rm = TRUE))
+      summarise(total_pop = sum(Pop_Weight, na.rm = TRUE))
   }
 
   # For Population A --------------------------------------------------------
@@ -44,7 +52,7 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
   denominator_a <- df %>%
     filter( {{Geography}} == LocalityA ) %>%
     group_by(!!!group_var) %>%
-    summarise(LocalityA_pop = n(),.groups = "drop"  )
+    summarise(LocalityA_pop = n(), .groups = "drop")
 
   # Population in Locality with Condition
 
@@ -52,27 +60,26 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
     filter( {{Geography}} == LocalityA ) %>%
     filter( {{Condition}} == 1) %>%
     group_by(!!!group_var) %>%
-    summarise(conditionA_pop = n(),.groups = "drop"  )
+    summarise(conditionA_pop = n(), .groups = "drop")
 
 
   # Crude Prevalence
 
-  final_a <- left_join(denominator_a,numerator_a) %>%
+  final_a <- left_join(denominator_a, numerator_a) %>%
     mutate(crude_rate = conditionA_pop / LocalityA_pop) %>%
-    mutate(crude_prevalence = (conditionA_pop / LocalityA_pop) * 1000 )  %>%
+    mutate(crude_prevalence = (conditionA_pop / LocalityA_pop) * 1000)  %>%
     # Age Standardised
     left_join(pop_standard) %>%
     mutate(Expected_Prevalence = crude_rate * Pop_Weight) %>%
-    rename("{{LocalityA}}_Pop" :=LocalityA_pop,
-           "{{Condition}}_Pop":= conditionA_pop)
+    rename("{{LocalityA}}_Pop" := LocalityA_pop,
+           "{{Condition}}_Pop" := conditionA_pop)
 
 
-  standard_rate_a <- left_join(denominator_a,numerator_a) %>%
+  standard_rate_a <- left_join(denominator_a, numerator_a) %>%
     mutate(crude_rate = conditionA_pop / LocalityA_pop) %>%
     left_join(pop_standard) %>%
     mutate(Expected_Prevalence = crude_rate * Pop_Weight) %>%
-    #drop_na() %>%
-    summarise((sum(Expected_Prevalence,na.rm = TRUE)/overall_pop_sum) * 1000,.groups = "drop") %>%
+    summarise((sum(Expected_Prevalence, na.rm = TRUE) / overall_pop_sum) * 1000, .groups = "drop") %>%
     pull()
 
   # For Population B --------------------------------------------------------
@@ -82,17 +89,17 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
   denominator_b <- df %>%
     filter( {{Geography}} == LocalityB ) %>%
     group_by(!!!group_var) %>%
-    summarise(LocalityB_pop = n(),.groups = "drop" )
+    summarise(LocalityB_pop = n(), .groups = "drop")
   # Population in Locality with Condition
 
   numerator_b <- df %>%
     filter( {{Geography}} == LocalityB ) %>%
     filter( {{Condition}} == 1) %>%
     group_by(!!!group_var) %>%
-    summarise(conditionB_pop = n(),.groups = "drop")
+    summarise(conditionB_pop = n(), .groups = "drop")
 
 
-  final_b<- left_join(denominator_b,numerator_b) %>%
+  final_b <- left_join(denominator_b, numerator_b) %>%
     mutate(crude_rate = conditionB_pop / LocalityB_pop) %>%
     mutate(crude_prevalence = (conditionB_pop / LocalityB_pop) * 1000) %>%
     left_join(pop_standard) %>%
@@ -101,11 +108,11 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
            "{{Condition}}_Pop" := conditionB_pop)
 
 
-  standard_rate_b <- left_join(denominator_b,numerator_b) %>%
+  standard_rate_b <- left_join(denominator_b, numerator_b) %>%
     mutate(crude_rate = conditionB_pop / LocalityB_pop) %>%
     left_join(pop_standard) %>%
     mutate(Expected_Prevalence = crude_rate * Pop_Weight) %>%
-    summarise((sum(Expected_Prevalence,na.rm = TRUE)/overall_pop_sum) * 1000,.groups = "drop") %>%
+    summarise((sum(Expected_Prevalence, na.rm = TRUE) / overall_pop_sum) * 1000, .groups = "drop") %>%
     pull()
 
 
@@ -119,11 +126,16 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
 
   }
 
-  message("Locality A Age Standardised Prevalence Rate: ",round(standard_rate_a,2))
+  message("Locality A Age Standardised Prevalence Rate: ",
+          round(standard_rate_a, 2))
 
-  message("Locality B Age Standardised Prevalence Rate: ",round(standard_rate_b,2))
+  message("Locality B Age Standardised Prevalence Rate: ",
+          round(standard_rate_b, 2))
 
-  message("The Comparative Age Standardised Prevalence Ratio of Locality A to Locality B is ",round(standard_rate_a/standard_rate_b,3))
+  message(
+    "The Comparative Age Standardised Prevalence Ratio of Locality A to Locality B is ",
+    round(standard_rate_a / standard_rate_b, 3)
+  )
 
 
   if(Verbose == TRUE){
@@ -134,6 +146,7 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
 
   }
 
+
   final_a$Location <- LocalityA
 
   final_b$Location <- LocalityB
@@ -142,9 +155,7 @@ compare_standardised_rates <- function(df, Geography, LocalityA, LocalityB, Cond
 
   final_b <- janitor::clean_names(final_b)
 
-  final_output <- list(final_a,final_b)
-
-  #return(final_output)
+  final_output <- list(final_a, final_b)
 
 }
 
