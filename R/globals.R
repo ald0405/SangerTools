60441c9924cfb883a1d232bbbbd17b6489639449
ald0405 <38423717+ald0405@users.noreply.github.com>
2022-01-11 16:26:05 +0000

    Added standardised rates function

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,4 @@
 utils::globalVariables(c("patients_n", "proportion", "condition_pop",
                          "demo_pop", "desc", "prevalence_1k",
-                         "prevalance_1k"))
+                         "prevalance_1k","Pop_Weight","conditionA_pop","LocalityA_pop",
+                         "Expected_Prevalence","crude_rate","conditionB_pop","LocalityB_pop"))
