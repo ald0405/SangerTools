6fe66456eb6d65612d760eee5e682da81fc02c43
ald0405 <38423717+ald0405@users.noreply.github.com>
2022-02-03 08:50:59 +0000

    Dataset UK 2018 age standardisation added

---
+++ R/uk_pop_standard.R
@@ -0,0 +1,10 @@
+#' Data set of 2018 UK Population
+#'
+#' Data is taken from ONS and is split into 5 year age band
+#' @format A tibble with 29 rows and 2 variables:
+#' \describe{
+#'   \item{UK_Population}{dbl Year price was recorded}
+#'   \item{Ageband}{5 Year age band for population}
+#'   }
+#' @source \url{https://www.pnas.org/content/114/7/1512/tab-figures-data}
+"uk_pop_standard"
